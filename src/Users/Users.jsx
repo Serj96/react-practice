44450923659203d93f630d237821baa2e62d0a50
Lysak-Anna <hvlysak@gmail.com>
2022-12-30 14:29:42 +0300

    update

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -1,33 +1,39 @@
 import { Component } from "react";
 import { nanoid } from "nanoid";
-import { UserList } from './UserList/UserList'
-import UserForm from './UserForm/UserForm'
-
+import { UserList } from "./UserList/UserList";
+import UserForm from "./UserForm/UserForm";
+const STORAGE_KEY = "users";
 class Users extends Component {
   state = {
     users: [],
     name: "",
     phone: "",
   };
-
-  
-  handleSubmit = (user) => {
-    const newUser = { ...user, id: nanoid() }
-    this.setState(prevState => ({ users: [...prevState.users, newUser] }))
+  componentDidMount() {
+    const users = localStorage.getItem(STORAGE_KEY);
+    const parsedUsers = JSON.parse(users);
+    if (parsedUsers?.length) {
+      this.setState({ users: parsedUsers });
+    }
   }
+  componentDidUpdate(_, prevState) {
+    if (prevState.users !== this.state.users) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.state.users));
+    }
+  }
+  handleSubmit = (user) => {
+    const newUser = { ...user, id: nanoid() };
+    this.setState((prevState) => ({ users: [...prevState.users, newUser] }));
+  };
 
   render() {
     const { users } = this.state;
     return (
-        <>
-            <UserForm onHandleSubmit={this.handleSubmit} />
-            <>
-                {users.length > 0 && <UserList users={users} />}
-            </>
-            
-        </>
+      <>
+        <UserForm onHandleSubmit={this.handleSubmit} />
+        <>{users.length > 0 && <UserList users={users} />}</>
+      </>
     );
-
   }
 }
 
--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -1,38 +1,48 @@
 import { Component } from "react";
 
-
 export default class Basket extends Component {
-    state = {
-        apple: 0,
-        milk: 0,
-        cheese: 0
-    }
-    total = () => {
-
-        return Object.values(this.state).reduce((acc, num) => acc + num);
-    }
-    onClick = (item) => {
-        console.log(item);
-        this.setState(prevState => ({
-            [item]: prevState[item] + 1
-        }))
-    }
-    render() {
-        const { apple, milk, cheese } = this.state;
-        const options = Object.keys(this.state);
-        console.log(options);
-        return <div>
-            <>
-                {options.map(item =>
-                    <button type="button" onClick={() => this.onClick(item)}>{item}</button>
-                )}
-            </>
-            {this.total() > 0 && <ul>
-                <li>Apple:<p>{apple}</p></li>
-                <li>milk:<p>{milk}</p></li>
-                <li>cheese:<p>{cheese}</p></li>
-                <li>total:{this.total()}</li>
-            </ul>}
-        </div>
-    }
+  state = {
+    apple: 0,
+    milk: 0,
+    cheese: 0,
+  };
+  total = () => {
+    return Object.values(this.state).reduce((acc, num) => acc + num);
+  };
+  onClick = (item) => {
+    console.log(item);
+    this.setState((prevState) => ({
+      [item]: prevState[item] + 1,
+    }));
+  };
+  render() {
+    const { apple, milk, cheese } = this.state;
+    const options = Object.keys(this.state);
+    console.log(options);
+    return (
+      <div>
+        <>
+          {options.map((item) => (
+            <button key={item} type="button" onClick={() => this.onClick(item)}>
+              {item}
+            </button>
+          ))}
+        </>
+        {this.total() > 0 && (
+          <ul>
+            <li>
+              Apple:<p>{apple}</p>
+            </li>
+            <li>
+              milk:<p>{milk}</p>
+            </li>
+            <li>
+              cheese:<p>{cheese}</p>
+            </li>
+            <li>total:{this.total()}</li>
+          </ul>
+        )}
+      </div>
+    );
+  }
 }
