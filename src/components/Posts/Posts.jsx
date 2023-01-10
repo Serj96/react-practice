9427d365e35ff49d191b8bc68aaa82c83ff680f8
Serj96 <99637869+Serj96@users.noreply.github.com>
2023-01-10 16:47:00 +0200

    ,

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,64 +1,118 @@
-import { Component } from "react";
-import getPosts from "services/API";
-import Loader from "components/Loader/Loader";
-import Failure from "components/Failure/Failure";
+import getPosts from 'services/API';
+import Loader from 'components/Loader/Loader';
+import { useState, useEffect } from 'react';
+import Failure from 'components/Failure/Failure';
 
-export default class Posts extends Component {
-  state = {
-    posts: [],
-    page: 1,
-    limit: 30,
-    loading: false,
-    error: null,
-  };
-  componentDidMount() {
-    this.fetchPosts();
-  }
-  componentDidUpdate(_, prevState) {
-    if (prevState.page !== this.state.page) {
-      this.fetchPosts();
-    }
-  }
-  async fetchPosts() {
-    const { page, limit } = this.state;
+const Posts = () => {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(10);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-    this.setState({ loading: true });
+  useEffect(() => {
+    fetchPost();
+  }, [page]);
+
+  const fetchPost = async () => {
+    setLoading(true);
     try {
       const data = await getPosts(page, limit);
-      this.setState((prevState) => ({ posts: [...prevState.posts, ...data] }));
+      setPosts(prev => [...prev, ...data]);
     } catch (error) {
-      this.setState({ error });
+      setError(error);
     } finally {
-      this.setState({ loading: false });
+      setLoading(false);
     }
-  }
-  onClickHandler = () => {
-    this.setState((prevState) => ({ page: prevState.page + 1 }));
   };
-  render() {
-    const { posts, loading, error } = this.state;
-    return (
-      <div>
-        {loading && <Loader />}
-        {error && <Failure error={error.message} />}
-        {posts.length > 0 && (
-          <>
-            <ul>
-              {posts.map(({ title, body }, index) => (
-                <li key={index}>
-                  <p>{title}</p>
-                  <p>{body}</p>
-                </li>
-              ))}
-            </ul>
-            {posts.length < 100 && (
-              <button type="button" onClick={this.onClickHandler}>
-                Load more
-              </button>
-            )}
-          </>
-        )}
-      </div>
-    );
-  }
-}
+
+  const onClickHandler = () => {
+    setPage(prev =>  prev + 1 );
+  };
+
+  return (
+    <div>
+      {loading && <Loader />}
+      {error && <Failure error={error.message} />}
+      {posts.length > 0 && (
+        <>
+          <ul>
+            {posts.map(({ title, body }, index) => (
+              <li key={index}>
+                <p>{title}</p>
+                <p>{body}</p>
+              </li>
+            ))}
+          </ul>
+          {posts.length < 100 && (
+            <button type="button" onClick={onClickHandler}>
+              Load more
+            </button>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Posts;
+
+// export default class Posts extends Component {
+//   state = {
+//     posts: [],
+//     page: 1,
+//     limit: 30,
+//     loading: false,
+//     error: null,
+//   };
+//   componentDidMount() {
+//     this.fetchPosts();
+//   }
+//   componentDidUpdate(_, prevState) {
+//     if (prevState.page !== this.state.page) {
+//       this.fetchPosts();
+//     }
+//   }
+//   async fetchPosts() {
+//     const { page, limit } = this.state;
+
+//     this.setState({ loading: true });
+//     try {
+//       const data = await getPosts(page, limit);
+//       this.setState((prevState) => ({ posts: [...prevState.posts, ...data] }));
+//     } catch (error) {
+//       this.setState({ error });
+//     } finally {
+//       this.setState({ loading: false });
+//     }
+//   }
+//   onClickHandler = () => {
+//     this.setState((prevState) => ({ page: prevState.page + 1 }));
+//   };
+//   render() {
+//     const { posts, loading, error } = this.state;
+//     return (
+//       <div>
+//         {loading && <Loader />}
+//         {error && <Failure error={error.message} />}
+//         {posts.length > 0 && (
+//           <>
+//             <ul>
+//               {posts.map(({ title, body }, index) => (
+//                 <li key={index}>
+//                   <p>{title}</p>
+//                   <p>{body}</p>
+//                 </li>
+//               ))}
+//             </ul>
+//             {posts.length < 100 && (
+//               <button type="button" onClick={this.onClickHandler}>
+//                 Load more
+//               </button>
+//             )}
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
