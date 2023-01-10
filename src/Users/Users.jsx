65e7a14662a4aaf01b2bd7f2e982f6ac2296bb6e
Lysak-Anna <hvlysak@gmail.com>
2023-01-10 14:39:21 +0300

    update class

--- src/Users/Users.jsx
+++ src/Users/Users.jsx
@@ -1,50 +1,78 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from "nanoid";
 import { UserList } from "./UserList/UserList";
 import { UserForm } from "./UserForm/UserForm";
+
 const STORAGE_KEY = "users";
-class Users extends Component {
-  state = {
-    users: [],
-    name: "",
-    phone: "",
+
+export default function Users() {
+  const [users, setUsers] = useState(
+    () => JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? []
+  );
+  useEffect(() => {
+    const usersToSave = JSON.stringify(users);
+    localStorage.setItem(STORAGE_KEY, usersToSave);
+  }, [users]);
+  const handelDelete = (id) => {
+    setUsers((prev) => prev.filter((user) => user.id !== id));
   };
-  componentDidMount() {
-    const users = localStorage.getItem(STORAGE_KEY);
-    const parsedUsers = JSON.parse(users);
-    if (parsedUsers?.length) {
-      this.setState({ users: parsedUsers });
-    }
-  }
-  componentDidUpdate(_, prevState) {
-    if (prevState.users !== this.state.users) {
-      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.state.users));
-    }
-  }
-  handleSubmit = (user) => {
+  const handleSubmit = (user) => {
     const newUser = { ...user, id: nanoid() };
-    this.setState((prevState) => ({ users: [...prevState.users, newUser] }));
+    setUsers((prev) => [...prev, newUser]);
   };
-
-  handelDelete = (id) => {
-    this.setState((prevState) => ({
-      users: prevState.users.filter((user) => user.id !== id),
-    }));
-  };
-
-  render() {
-    const { users } = this.state;
-    return (
+  return (
+    <>
+      <UserForm onHandleSubmit={handleSubmit} />
       <>
-        <UserForm onHandleSubmit={this.handleSubmit} />
-        <>
-          {users.length > 0 && (
-            <UserList users={users} onHandelDelete={this.handelDelete} />
-          )}
-        </>
+        {users.length > 0 && (
+          <UserList users={users} onHandelDelete={handelDelete} />
+        )}
       </>
-    );
-  }
+    </>
+  );
 }
+// class Users extends Component {
+//   state = {
+//     users: [],
+//     name: "",
+//     phone: "",
+//   };
+//   componentDidMount() {
+//     const users = localStorage.getItem(STORAGE_KEY);
+//     const parsedUsers = JSON.parse(users);
+//     if (parsedUsers?.length) {
+//       this.setState({ users: parsedUsers });
+//     }
+//   }
+//   componentDidUpdate(_, prevState) {
+//     if (prevState.users !== this.state.users) {
+//       localStorage.setItem(STORAGE_KEY, JSON.stringify(this.state.users));
+//     }
+//   }
+//   handleSubmit = (user) => {
+//     const newUser = { ...user, id: nanoid() };
+//     this.setState((prevState) => ({ users: [...prevState.users, newUser] }));
+//   };
 
-export default Users;
+//   handelDelete = (id) => {
+//     this.setState((prevState) => ({
+//       users: prevState.users.filter((user) => user.id !== id),
+//     }));
+//   };
+
+//   render() {
+//     const { users } = this.state;
+//     return (
+//       <>
+//         <UserForm onHandleSubmit={this.handleSubmit} />
+//         <>
+//           {users.length > 0 && (
+//             <UserList users={users} onHandelDelete={this.handelDelete} />
+//           )}
+//         </>
+//       </>
+//     );
+//   }
+// }
+
+// export default Users;
